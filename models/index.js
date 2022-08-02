d75d73a0a2e30b0058f0f20d4f47f54de604f0df
C K <ctkoo@ucsc.edu>
2022-08-02 03:04:39 -0700

    readme

--- models/index.js
+++ models/index.js
@@ -8,20 +8,22 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through : ProductTag,
+  through: ProductTag,
   foreignKey: 'product_id',
-
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id',
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
